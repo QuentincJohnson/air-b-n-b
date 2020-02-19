9ec50d7b809fed536416b5960b78df1e2fe9858c
QuentincJohnson <quentincjohnson@gmail.com>
2020-02-18 19:52:31 -0500

    made listings on a diffrent page (complete)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");//uses express
 const exphbs  = require('express-handlebars');// uses express handlebars
 const bodyParser = require('body-parser');// uses body parser
 
+const listMod = require('./models/listings-list') //not a thiird party packge and gets special treatment add ./
+
 const app = express();
 
 app.engine('handlebars', exphbs());//awknowladges express handlebars
@@ -35,55 +37,9 @@ app.get("/registration", (req,res)=>{
 
 app.get("/listing", (req,res)=>{
 
-    const listings = [
-        {
-            location: "Canada,ON",
-            name:"Downtown Condo with View",
-            rateing:"4.8",
-            price:"96",
-            img:"/img/condo-1.jpg"
-        },
-        {
-            location: "Canada,QC",
-            name:"Bungalow in Montreal",
-            rateing:"4.7",
-            price:"78",
-            img:"/img/bung.jpg"
-        },
-        {
-            location: "Canada,BC",
-            name:"4 Person Beachhouse",
-            rateing:"4.8",
-            price:"146",
-            img:"/img/beachhouse.jpg"
-        },
-        {
-            location: "Canada,ON",
-            name:"Penthouse Premium",
-            rateing:"4.9",
-            price:"160",
-            img:"/img/penthouse.jpg"
-        },
-        {
-            location: "Canada,ON",
-            name:"Ottowa Basment",
-            rateing:"4.5",
-            price:"45",
-            img:"/img/basement.jpg"
-        },
-        {
-            location: "Canada,BC",
-            name:"1st floor 2 person appartment",
-            rateing:"4.6",
-            price:"56",
-            img:"/img/apt.jpg"
-        },
-
-    ];
-
     res.render("listing", {
         title: "Air BnB Listing",
-        places: listings.places
+        places: listMod.places
     });
 });
 
